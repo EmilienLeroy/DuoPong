563123ace8aefa992afe197f6632a894983b96c4
Emilien Leroy <emilien1005@hotmail.fr>
2023-03-17 06:57:14 +0100

    ✨ Add scores and respawn the once a goal is score

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -3,13 +3,16 @@ extends Node2D
 var Player = preload("res://entities/player/player.tscn");
 var Ball = preload("res://entities/ball/ball.tscn");
 var offset = 100;
+var scores = [0, 0];
 
 func _ready():
 	randomize();
 
 	add_player(true);
 	add_player(false);
-	add_ball(Vector2(rand_range(0.5, 5), get_random_direction()));
+	add_ball(get_random_direction());
+	
+	$Walls.connect("goal", self, "on_goal");
 	pass
 
 
@@ -32,12 +35,28 @@ func add_ball(direction):
 	var ball = Ball.instance();
 	
 	ball.init(direction, get_viewport_rect().size / 2);
-	add_child(ball);
+	call_deferred("add_child", ball);
 	
 	return ball;
 
 func get_random_direction():
+	var x = rand_range(0.5, 5);
+	
 	if randi() % 2 == 0:
-		return 1
+		return Vector2(x, 1);
 	else:
-		return -1
+		return Vector2(x, -1);
+		
+
+func increase_score(scores, goal):
+	scores[goal] = scores[goal] + 1; 
+
+func update_scores(scores):
+	$Canvas/ScoreBottom.text = str(scores[0]);
+	$Canvas/ScoreTop.text = str(scores[1]);
+
+func on_goal(ball, goal):
+	ball.queue_free();
+	add_ball(get_random_direction());
+	increase_score(scores, goal);
+	update_scores(scores);
