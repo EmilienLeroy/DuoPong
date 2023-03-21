0c7daf6d1b4bf86e57cdf04b3651fb072aead5d7
Emilien Leroy <emilien1005@hotmail.fr>
2023-03-21 07:05:03 +0100

    ✨ Increase speed of the ball on each collision

--- entities/ball/ball.gd
+++ entities/ball/ball.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 export var speed = 300;
+export var max_speed = 1000;
+export var increase_speed = 30;
 var direction = Vector2.ZERO;
 
 func init(d, p):
@@ -12,5 +14,10 @@ func _physics_process(delta):
 	
 	if (!collision_info):
 		return;
-
-	direction = direction.bounce(collision_info.normal)
+	
+	direction = direction.bounce(collision_info.normal);
+	
+	if (speed > max_speed):
+		return;
+		
+	speed = speed + increase_speed;
