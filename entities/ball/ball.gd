390bdaf62b3764565127ad3011dd37f7d28d70fa
Emilien Leroy <emilien1005@hotmail.fr>
2023-03-23 07:08:51 +0100

    ✨ Add particle when ball collide to a wall or a player

--- entities/ball/ball.gd
+++ entities/ball/ball.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
+var CollisionParticle = preload("res://entities/ball/particles/collision.tscn");
+
 export var speed = 350;
 export var max_speed = 1000;
 export var increase_speed = 30;
 var direction = Vector2.ZERO;
-var trail_length = 25
 
 func init(d, p):
 	direction = d.normalized();
@@ -17,12 +18,22 @@ func _physics_process(delta):
 		return;
 	
 	direction = direction.bounce(collision_info.normal);
+	add_collision_particles();
 	
 	if (speed > max_speed):
 		return;
 		
 	speed = speed + increase_speed;
 
+func add_collision_particles():
+	var particles = CollisionParticle.instance();
+	
+	add_child(particles);
+	particles.initial_velocity = speed / 3;
+	particles.emitting = true;
+	
+	return particles;
+	
 func destroy():
 	yield(get_tree().create_timer(5.0), "timeout");
 	queue_free();
